2fa660bb7e192d95aee4702e6b07392e5d5990f1
Emanuel <123523520+emanueldias01@users.noreply.github.com>
2025-08-02 17:39:04 -0300

    controller done

--- src/controllers/EventController.ts
+++ src/controllers/EventController.ts
@@ -1,4 +1,29 @@
 import { Request, Response } from "express";
-import fs from 'fs'
+import EventService from "../services/EventService";
+import { calendar_v3 } from "googleapis";
 
+class EventController{
+    static async getEvents(req : Request, res : Response){
+        const data = await EventService.listEvents();
+        res.status(200).json(data);
+    }
 
+    static async createEvent(req : Request, res : Response){
+        const dataEvent : calendar_v3.Schema$Event = req.body;
+        const responseCreate = await EventService.createEvent(dataEvent);
+        res.status(201).json(responseCreate);
+    }
+
+    static async updateEvent(req : Request, res : Response){
+        const eventId : string = req.params.id;
+        const dataEvent : calendar_v3.Schema$Event = req.body;
+        const responseUpdate = await EventService.updateEvent(eventId, dataEvent);
+        res.status(200).json(responseUpdate);
+    }
+
+    static async deleteEvent(req : Request, res : Response){
+        const eventId = req.params.id;
+        await EventService.deleteEvent(eventId);
+        res.status(204).send();
+    }
+}
